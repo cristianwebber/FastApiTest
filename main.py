46c0837f54ae076f6b792852a2413e63c2412d98
Cristian Webber <cbwebbers@gmail.com>
2021-09-07 07:22:21 -0300

    Reorganize directory

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-
+from app import  models
+from app.database import engine
+from app.routers import blog, user, authentication
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
-@app.get('/blog')
-def index(limit=10, published: bool = True, sort: Optional[str] = None):
-    # only get 10 published blogs
-    if published:
-        return {'data': f'{limit} published blogs from the db'}
-    else:
-        return {'data': f'{limit} blogs from the db'}
-
-
-@app.get('/blog/unpublished')
-def unpublished():
-    return {'data': 'all unpublished blogs'}
-
-
-@app.get('/blog/{id}')
-def show(id: int):
-    # fetch blog with id = id
-    return {'data': id}
-
-
-@app.get('/blog/{id}/comments')
-def comments(id, limit=10):
-    # fetch comments of blog with id = id
-    return {'data': {'1', '2'}}
-
-
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: Optional[bool]
-
-
-@app.post('/blog')
-def create_blog(blog: Blog):
-    return {'data': f"Blog is created with title as {blog.title}"}
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
